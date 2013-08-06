491a4e2e7f3a378e44a37a3cec21565ca45b2e7c
Max Ogden <max@maxogden.com>
2013-08-06 14:59:56 -0700

    1.1.0 - add .destroy

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var sublevel = require('level-sublevel')
 var multilevel = require('multilevel')
 var createPersona = require('persona-id')
 var request = require('browser-request')
+var deleteRange = require('level-delete-range')
 
 module.exports = function(options) {
   if (!options) options = {}
@@ -35,6 +36,11 @@ User.prototype.getProfile = function(cb) {
   })
 }
 
+User.prototype.destroy = function(subdb, cb) {
+  var prefix = subdb.prefix()
+  deleteRange(subdb, {start: prefix, end: prefix + '\xff'}, cb)
+}
+
 User.prototype.remote = function(name) {
   var backend = this.options.baseURL.replace('http:', 'ws:') + '/' + name
   var stream = websocket(backend)
@@ -46,8 +52,12 @@ User.prototype.remote = function(name) {
 }
 
 User.prototype.copy = function(from, to, cb) {
+  var read = from.createReadStream({valueEncoding: 'binary'})
   var write = to.createWriteStream({valueEncoding: 'binary'})
-  from.createReadStream({valueEncoding: 'binary'}).pipe(write)
+  write.on('data', log)
+  read.on('data', log)
+  function log(c) { console.log(c) }
+  read.pipe(write)
   var sent = false
   write.on('close', function() {
     if (!sent) {
